18b26d842db7550de6498e46fff622c75bef2192
Richard Remer <reanjr@gmail.com>
2012-06-03 21:12:55 -0700

    added connection info to mysql data store class

--- index.php
+++ index.php
@@ -3,4 +3,10 @@
  * Copyright Skinit, Inc.
  */
 
-$catalog = new MysqlDataStore();
+$catalog = new MysqlDataStore( 'db-dev', 'dba_user', 'foo', 'catalog' );
+Orm::generateModels( $catalog, array(
+    'base_dir' => 'base',
+    'model_dir' => 'model',
+    
+    
+) );
