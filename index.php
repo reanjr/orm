4a8aad502d72374ac755c619a2db21c17633cef7
rremer <rremer@minime>
2012-06-17 20:20:37 -0700

    created an iterator for Sqlite results

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
  * Copyright Skinit, Inc.
  */
 
+require_once( 'autoload.inc' );
+
+// read config
 $configFile = file_get_contents( 'orm_config.json' );
 $config = json_decode( $configFile, true );
+
+// connect to data store
 $data = new SqliteDataStore( "test/example.db" );
+$data->connect();
+
+// create model files
 Orm::generateModels( $data, $config );
