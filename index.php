9be3873bb946f2bba6bc08a640954097acddb61f
Richard Remer <reanjr@gmail.com>
2012-07-23 08:15:50 -0700

    half broken work on keyless access

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once( 'autoload.inc' );
 $configFile = file_get_contents( 'orm_config.json' );
 $config = json_decode( $configFile, true );
 
-// register the data store and get a connection for it
+// register the data store and get its instance
 Orm::registerDataStore( "Example", "sqlite", array( "test/example.db" ) );
 $dataStore = Orm::getDataStore( "Example" );
 
